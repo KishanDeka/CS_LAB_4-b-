13f001ac16a6cac8ea701b0d4b3d721b64766e80
KishanDeka <31157122+KishanDeka@users.noreply.github.com>
2017-10-04 15:04:10 +0530

    Update question13.cpp

--- question13.cpp
+++ question13.cpp
@@ -2,16 +2,16 @@
 #define SIZE 4
 using namespace std;
  
-void goTrough(int arr[SIZE][SIZE],int r, int c){
+void g(int arr[SIZE][SIZE],int r, int c){
     if(r==SIZE){
         return;
     }
-    if(c==SIZE) return goTrough(arr, r+1, 0);
+    if(c==SIZE) return g(arr, r+1, 0);
 	cout<<arr[r][c]<<" "; 
-    return goTrough(arr,r,c+1);
+    return g(arr,r,c+1);
 }
 int main(){
-    int arre[SIZE][SIZE]={{1,2,3,4},{50,101,63,200},{50,240,600,300},{700,400,399,385}};//9
-    goTrough(arre,0,0);
+    int arre[SIZE][SIZE]={{1,2,3,4},{50,101,63,200},{50,240,600,300},{700,400,399,385}};
+    g(arre,0,0);
     return 0;  
 }
