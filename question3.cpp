a6da81813971ac59da70b0192c1a5b4204ceb23c
KishanDeka <31157122+KishanDeka@users.noreply.github.com>
2017-10-05 22:19:03 +0530

    Update question3.cpp

--- question3.cpp
+++ question3.cpp
@@ -1,21 +1,32 @@
 #include <iostream>
 using namespace std;
 
-void print(int l, int u)
-{   if(l<u && l%2==0)
-    cout<< l<<", ";
-    print(l + 1, u);
+void printeven(int l, int u)
+{   if(l<=u )
+        { if (l%2==0){cout<< l<<", ";};
+         printeven(l + 1, u);}
+}
+void printodd(int l, int u)
+{   if(l<=u )
+        { if (l%2!=0){cout<< l<<", ";};
+         printodd(l + 1, u);}
 }
 int main()
 {
     int x, y;
-
+    char ch;
     cout << "enter the lower limit : ";
     cin>> y;
     cout<< "enter the upper limit : ";
     cin >> x;
-    cout << "the even number between "<<y<<" and "<<x<<" are :"<<endl;
-    print(y, x);
+    cout << "To print odd numbers type 'o' or to print even number type 'e' : "<<endl;
+    cin >> ch;
+    if (ch=='o')
+        {  cout<<"All odd numbers from " <<y<<" to "<<x<<" are :"<<endl;
+            printodd(y, x);}
+    if (ch=='e')
+        {   cout<<"All even numbers from " <<y<<" to "<<x<<" are :"<<endl;
+            printeven(y,x);}
 
     return 0;
 }
